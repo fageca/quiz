6e33b4dad5f4145e4071223cc96ffceb4a8fa7ec
fageca <David Segui>
2015-07-13 23:01:15 +0200

    Crear pagina de author

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ var quizController = require('../controllers/quiz_controller.js');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+router.get('/author', function(req, res) {
+  res.render('author');
+});
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+
 module.exports = router;
