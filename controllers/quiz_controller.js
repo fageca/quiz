03f85d37858062b107a939d0911b32b481d4bc61
fageca <David Segui>
2015-07-26 16:03:55 +0200

    Creación del Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,41 @@
 var models = require('../models/models.js');
 
-//Get /quizes/question
-exports.question = function(req,res){
-	models.Quiz.findAll().success(function(quiz) {
-		res.render('quizes/question',{pregunta:quiz[0].pregunta})
-	})
+
+//Autoloas
+exports.load = function(req, res, next, quizId) {
+    //console.log('hola1');
+    models.Quiz.find(quizId).success(
+            function (quiz){
+                if (quiz){
+                    //console.log(quiz);
+                    req.quiz = quiz;
+                    next();
+                } else { next (new Error('No existe quizId='+quizId));}
+            }
+         )//.catch(function(error){next(error);});   
 };
+
+//Get /quizes/
+exports.index = function(req,res){
+
+        	models.Quiz.findAll().success(function(quizes) {
+		res.render('quizes/index.ejs',{quizes:quizes});
+	})//.catch(function(error) { next(error); })
+        
+    
+};
+
+
 //Get /quizes/answer
+exports.show = function(req,res){
+    //console.log(req.quiz);
+    res.render('quizes/show',{quiz: req.quiz});
+};
+
 exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz) {
-		if ((req.query.respuesta)===quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta:'Correcto'});
-		}
-		else{
-			res.render('quizes/answer',{respuesta:'Incorrecto'});
-		}
-	})
+    var resultado ='Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta){
+        resultado='Correcto';
+    }
+    res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,17 @@ var quizController = require('../controllers/quiz_controller.js');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
 router.get('/author', function(req, res) {
   res.render('author');
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//Autoload
+router.param('quizid',quizController.load);
+
+//Definicion de rutas de /quiz
+router.get('/quizes',quizController.index);
+router.get('/quizes/:quizid(\\d+)',quizController.show);
+router.get('/quizes/:quizid(\\d+)/answer',quizController.answer);
 
 module.exports = router;
